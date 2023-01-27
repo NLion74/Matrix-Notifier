9ad80f277822e85500472443127c2803eb0eb8b7
NLion74 <97677226+NLion74@users.noreply.github.com>
2023-01-27 11:47:44 +0100

    Changed Bot Creds

--- bot/main.py
+++ bot/main.py
@@ -4,15 +4,21 @@ from asyncio import sleep
 
 import sync
 
+# Server
 host = "127.0.0.1"
 port = "5505"
 url = f"{host}:{port}"
 scheme = "http://"
 
+# Bot Creds
+bot_name = "@bot_user:home.server"
+bot_pass = "bot_pass"
+home_server = "https://home.server"
+
 
 async def main():
-    client = AsyncClient("https://mtx.nlion.nl", "@youtube-downloader:nlion.nl")
-    print(await client.login("@Q@G8E8xiAFL&w74"))
+    client = AsyncClient(f"{home_server}", f"{bot_name}")
+    print(await client.login(f"{bot_pass}"))
 
     while True:
         await sync.sync(scheme, url, client)
