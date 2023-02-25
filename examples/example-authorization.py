db38e699fbdcc124a2288fa57c4bf5bb38906bcd
NLion74 <nlion7444@gmail.com>
2023-02-25 11:59:24 +0100

    Fixed markdown and authorization example

--- examples/example-authorization.py
+++ examples/example-authorization.py
@@ -11,12 +11,12 @@ auth_secret = ""
 # Note that you'll have to manually accept the room invite. Also Room Encryption is currently not supported.
 # To obtain the room id of a room under element, just right-click the room, and then you can find then under Advanced,
 # there is the "Internal room ID", just copy that, and you have successfully obtained the room_id and can paste it here.
-room_id = ""
+room_id = "!IuYrvSwpWqfPXsFqJL:nlion.nl"
 
 try:
     # You have to encode the data in utf-8 if you're using utf-8
     # It also works without encoding it, but I recommend always encoding it with utf-8 when possible
-    res = requests.post(matrix_notifier_url, headers={"Channel": "", "Authorization": auth_secret}, data="Hello, I am working even with authorization.".encode("utf-8"))
+    res = requests.post(matrix_notifier_url, headers={"Channel": room_id, "Authorization": auth_secret}, data="Hello, I am working even with authorization.".encode("utf-8"))
     if res.status_code == 401:
         print("The auth_secret seems to be invalid")
 except requests.exceptions.RequestException:
--- examples/example-markdown.py
+++ examples/example-markdown.py
@@ -10,18 +10,18 @@ markdown = "true"
 # Note that you'll have to manually accept the room invite. Also Room Encryption is currently not supported.
 # To obtain the room id of a room under element, just right-click the room, and then you can find then under Advanced,
 # there is the "Internal room ID", just copy that, and you have successfully obtained the room_id and can paste it here.
-room_id = ""
+room_id = "!IuYrvSwpWqfPXsFqJL:nlion.nl"
 
 message = "# Such a cool header\n" \
-          "Markdown is so **cool**." \
-          "it can even do *italic*." \
-          "> Wow, qoutes too." \
+          "Markdown is so **cool**.\n" \
+          "it can even do *italic*.\n" \
+          "> Wow, qoutes too.\n" \
           "`Even code blocks are working`"
 
 try:
     # You have to encode the data in utf-8 if you're using utf-8
     # It also works without encoding it, but I recommend always encoding it with utf-8 when possible
-    res = requests.post(matrix_notifier_url, headers={"Channel": "", "Markdown": markdown}, data=message.encode("utf-8"))
+    res = requests.post(matrix_notifier_url, headers={"Channel": room_id, "Markdown": markdown}, data=message.encode("utf-8"))
     if res.status_code == 401:
         print("The auth_secret seems to be invalid")
 except requests.exceptions.RequestException:
